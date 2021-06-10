110b298b5d1b41bf90e3b0ed317e258c62b40276
Greg Hu <guangyue.hu@sumup.com>
2021-06-10 23:20:19 +0200

    update parameters

--- main.tf
+++ main.tf
@@ -1,8 +1,6 @@
 module "test_instance" {
   source = "./ec2"
 
-  zone_id            = var.zone_id
-  subdomain          = var.subdomain
   aws_region         = var.aws_region
   name_tag           = var.name_tag
   vpc_security_group = module.vpc_sg.security-group-id
--- variable.tf
+++ variable.tf
@@ -1,11 +1,3 @@
-variable "zone_id" {
-  default = "Z07374591FC76OBQXEXUL"
-}
-
-variable "subdomain" {
-  default = "training.visiontech.com.sg"
-}
-
 variable "aws_region" {
   description = "The AWS region in which the resources will be created."
   type        = string
