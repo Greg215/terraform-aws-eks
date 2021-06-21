8c8e75b794bc34d17f545225e90aa1799335344c
Greg Hu <guangyue.hu@sumup.com>
2021-06-21 23:50:44 +0200

    all code intergate

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
-//module "test_instance" {
-//  source = "./ec2"
-//
-//  aws_region         = var.aws_region
-//  name_tag           = var.name_tag
-//  vpc_security_group = module.vpc_sg.security-group-id
-//  subnet_id = element(module.subnets.public_subnet_ids, 0)
-//}
-//
-//module "vpc_sg" {
-//  source = "./sg"
-//
-//  vpc_id   = module.vpc.vpc_id
-//  name_tag = var.sg_name_tag
-//}
+module "test_instance" {
+  source = "./ec2"
+
+  aws_region         = var.aws_region
+  name_tag           = var.name_tag
+  vpc_security_group = module.vpc_sg.security-group-id
+  subnet_id = element(module.subnets.public_subnet_ids, 0)
+}
+
+module "vpc_sg" {
+  source = "./sg"
+
+  vpc_id   = module.vpc.vpc_id
+  name_tag = var.sg_name_tag
+}
 
 locals {
   # enforce usage of eks_worker_ami_name_filter variable to set the right kubernetes version for EKS workers,
