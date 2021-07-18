e9df735c08d8198ae4e0700e20a7db37135033cf
Greg Hu <guangyue.hu@sumup.com>
2021-07-18 18:49:20 +0200

    update backend

--- provider.tf
+++ provider.tf
@@ -5,7 +5,7 @@
 
 terraform {
   backend "s3" {
-    bucket = "terraform-state-devops-training-bucket"
+    bucket = "terraform-state-scb-bucket"
     key    = "greg-demo/terraform.tfstate"
     region = "ap-southeast-1"
   }
